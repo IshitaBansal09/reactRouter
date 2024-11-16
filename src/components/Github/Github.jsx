eebaedaafbc60277afa8d9ef87ddc0ca07804ccf
Ishita Bansal <bansalishita59@gmail.com>
2024-11-16 16:20:59 +0530

    fix

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -25,6 +25,6 @@ function Github(){
 export default Github
 
 export const githubInfoLoader = async () => {
-    const response = await fetch('https://api.github.com/users/ishita-bansal1')
+    const response = await fetch('https://api.github.com/users/IshitaBansal09')
     return response.json()
 }
